1728c7e7e0401b5d9bc3b094a70891a7c08a47ed
julian <jdramirezl@eafit.edu.co>
2023-04-20 00:07:12 -0500

    Changeees

--- src/internal/connection.py
+++ src/internal/connection.py
@@ -9,25 +9,27 @@ class Connection:
         self.client_sockets = []
     
     def set_server_socket(self):
-        self.server_socket = self.start_server()
+        self.start_server()
     
     def set_gui(self, gui):
         self.chat_gui = gui
 
     def send_message(self, message):
         for client_socket in self.client_sockets:
-            client_socket.send(message.encode())
+            encoded_message = bytes(message, "utf-8")
+            client_socket.send(encoded_message)
 
     def receive_message(self, client_socket):
         while True:
             data = client_socket.recv(1024)
-            if not data or (data and data.decode() == "quit"):
+            if not data:
+                print("Closing connection to " + str(client_socket.getpeername()))
                 break
             self.chat_gui.add_message("Other: " + data.decode())
 
     def start_server(self):
         host_mac = "00:1A:7D:DA:71:13" # Replace with the MAC address of your Bluetooth adapter
-        port = 3
+        port = 7
         backlog = 1
         
         self.server_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
@@ -39,22 +41,21 @@ class Connection:
         
         accept_thread = threading.Thread(target=self.accept_connections)
         accept_thread.start()
+        
     
     def start_client(self):
         target_mac = "14:F6:D8:32:D4:89" # Replace with the MAC address of your Bluetooth adapter
-        port = 3
+        port = 7
         
-        self.client_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-        self.client_socket.connect((target_mac, port))
-        print(self.chat_gui, "exists")
-        receive_thread = threading.Thread(target=self.chat_gui.receive_messages)
-        receive_thread.start()
+        client_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        client_socket.connect((target_mac, port))
+        self.client_sockets.append(client_socket)
+        self.chat_gui.add_message("Connected to " + str(target_mac))
 
     def accept_connections(self):
         while True:
             client_socket, client_address = self.server_socket.accept()
-            self.chat_gui.add_message("Connected to " + str(client_address))
-            self.client_sockets.append(client_socket)
+            self.chat_gui.add_message("Connected from " + str(client_address))
             receive_thread = threading.Thread(target=self.receive_message, args=(client_socket,))
             receive_thread.start()
     
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-from internal.gui import GUI
+
+    connection = Connection()
